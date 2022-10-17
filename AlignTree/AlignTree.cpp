413970645f452cb88dc1d5bde175649bd7c213b7
Gleb Nikolsky <glebnik@outlook.com>
2022-10-17 18:33:01 +0300

    1

--- AlignTree/AlignTree.cpp
+++ AlignTree/AlignTree.cpp
@@ -46,44 +46,49 @@ int main(int argc, char* argv[])
 	opt.add_options()
 		("help,h", "Обязательные параметры:\nmaster-dir slave-dir \nХотя бы один из normalize-names align-dir")
 		("delimiter,d", po::value<char>(&delimiter)->default_value('#'), "Разделитель. По умолчанию '#'")
-		("master-dir,m", po::value(&master_dir), "Директория, по которой согласовывают или которую нормализуют")
-		("slave-dir,s", po::value(&slave_dir), "Директория, которую согласовывают или которую нормализуют")
+		("master-dir,m", po::value<string>(&master_dir), "Директория, по которой согласовывают или которую нормализуют")
+		("slave-dir,s", po::value<string>(&slave_dir), "Директория, которую согласовывают или которую нормализуют")
 		("normalize-names,n", "Нормализовать файлы в master-dir")
 		("align-dir,a", "Согласовать slave-dir с master-dir");
 		("replace,r", po::value< vector<string> >(), "Что найти, на что заменить");
 		("replace-regular,g", po::value< vector<string> >(), "Что найти(регулярное), на что заменить, номер группы");
 	po::variables_map vm;
 	try {
-		store(parse_command_line(argc, argv, opt), vm);
+		po::store(po::parse_command_line(argc, argv, opt), vm);
+		po::notify(vm);
 	}
 	catch (exception& e) {
 		cerr << e.what();
 		return 1;
 	}
-	//{
-	//	vector<int> height{ 1,8,6,2,5,4,8,3,7 };
-	//	int k{2};
-	//	int n{ (int)height.size() };
-	//	vector<int> pos(k);
-	//	generate(pos.begin(), pos.end(), []() {static int i{ 0 };  return i++; });
-	//	auto first = pos.begin();
-	//	auto last = pos.end();
-	//	int square{ 0 };
-	//	int tmp = min(height[pos[0]], height[pos[1]]) * abs(pos[1] - pos[0]);
-	//	square = max(tmp, square);
-	//	cout << '\t' << n << '\t' << k <<'\n';
-	//	while (*first != n - k ) {
-	//		auto mt = last;
-	//		while (*(--mt) == n - (last - mt) - 1);
-	//		(*mt)++;
-	//		while (++mt != last) *mt = *(mt - 1) + 1;
-	//		for (auto& i : pos) cout << i << ' ';
-	//		cout << '\n';
-	//		tmp = min(height[pos[0]], height[pos[1]]) * abs(pos[1] - pos[0]);
-	//		square = max(tmp, square);
-	//	}
-	//	cout << square;
-	//}
+	{
+		vector<int> nums1{ 1,3 };
+		vector<int> nums2{ 2 };
+		//https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+
+
+		//int n{ (int)height.size() };
+		//vector<int> pos(k);
+		//generate(pos.begin(), pos.end(), []() {static int i{ 0 };  return i++; });
+		//auto first = pos.begin();
+		//auto last = pos.end();
+		//int square{ 0 };
+		//int tmp = min(height[pos[0]], height[pos[1]]) * abs(pos[1] - pos[0]);
+		//square = max(tmp, square);
+		//cout << '\t' << n << '\t' << k <<'\n';
+		//while (*first != n - k ) {
+		//	auto mt = last;
+		//	while (*(--mt) == n - (last - mt) - 1);
+		//	(*mt)++;
+		//	while (++mt != last) *mt = *(mt - 1) + 1;
+		//	for (auto& i : pos) cout << i << ' ';
+		//	cout << '\n';
+		//	tmp = min(height[pos[0]], height[pos[1]]) * abs(pos[1] - pos[0]);
+		//	square = max(tmp, square);
+		//}
+		//cout << square;
+	}
 
 	if (vm.count("help") || !(!(vm.count("align-dir") && vm.count("master-dir") && vm.count("align-dir")) || !(vm.count("normalize-names") && vm.count("master-dir"))
 		|| !(vm.count("replace") || vm.count("master-dir")) || !(vm.count("replace-regular") && vm.count("master-dir")))) {
@@ -91,7 +96,7 @@ int main(int argc, char* argv[])
 		return 0;
 	}
 	try {
-		for (auto f = fs::recursive_directory_iterator(fs::path("C:\\Users\\glebn\\OneDrive\\Books")); f != fs::recursive_directory_iterator(); ++f)
+		for (auto f = fs::recursive_directory_iterator(fs::path(master_dir)); f != fs::recursive_directory_iterator(); ++f)
 			master_files.emplace_back(f->path().string());
 		if (vm.count("align-dir")) {
 			for (auto f = fs::recursive_directory_iterator(fs::path(slave_dir)); f != fs::recursive_directory_iterator(); ++f)  slave_files.emplace_back(f->path().string());
