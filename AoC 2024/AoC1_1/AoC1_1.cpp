ed02f6abe5a7391fdc820b06619fdbbd030d8b41
Gleb Nikolsky <glebnik@outlook.com>
2024-12-02 02:35:42 +0300

    Update AoC1_1.cpp

--- AoC 2024/AoC1_1/AoC1_1.cpp
+++ AoC 2024/AoC1_1/AoC1_1.cpp
@@ -12,26 +12,32 @@ using namespace std;
 int main()
 {
     ifstream input("1_1.txt");
+#ifdef PART1
     vector<long> l, r;
     while (!input.eof()) {
-        long val;
-        input >> val;
-        l.push_back(val);
-        input >> val;
-        r.push_back(val);
+        long lval,rval;
+        input >> lval >> rval;
+        l.push_back(lval);
+        r.push_back(rval);
     }
     sort(l.begin(), l.end());
     sort(r.begin(), r.end());
     long long res1{ 0 };
-    for (auto i = l.begin(), j = r.begin(); i != l.end(); ++i, ++j)
-        res1 += abs(*j - *i);
+    for (auto i = l.begin(), j = r.begin(); i != l.end(); ++i, ++j) res1 += abs(*j - *i);
     cout << res1 << '\n';
-// part 2 
+#else // part 2 
     long long res2{ 0 };
-    map<long, long> m;
-    for (auto& i : r) ++m[i];
-    for (auto& i : l) if (m.count(i)) res2 += i * m[i];
+    map<long, long> lm,rm;
+    while (!input.eof()) {
+        long lval, rval;
+        input >> lval >> rval;
+        ++lm[lval];
+        ++rm[rval];
+    }
+    for (auto& i : lm) 
+        if (rm.count(i.first)) 
+            res2 += i.first * rm[i.first]*i.second;
     cout << res2 << '\n';
-
+#endif
 }
 
